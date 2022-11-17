f55dbe56dda20ec2b8bf81d2d66c0666151efc48
Duckels L (FCES) <23046802@students.southwales.ac.uk>
2022-11-17 16:21:56 +0000

    Added more calculator functions

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,8 @@
 a = int(input("What would you like your first number to be?"))
 b = int(input("What would you like your second number to be?"))
 
-c = a + b
+add = a + b
+sub = a - b
+times = a*b 
+div = a/b 
+ratio = a/add
